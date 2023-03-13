4bc2f4c626573862d58a38ab7d2a255292951fad
Illia <illia.vasiliev@gmail.com>
2023-03-13 08:53:02 +0200

    start task 1 style

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,7 +1,8 @@
+import { List } from './FeedbackOptions.styled';
 import { nanoid } from 'nanoid';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <ul>
+  <List>
     {options.map(name => (
       <li key={nanoid()}>
         <button name={name} type="button" onClick={() => onLeaveFeedback(name)}>
@@ -9,5 +10,5 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
         </button>
       </li>
     ))}
-  </ul>
+  </List>
 );
